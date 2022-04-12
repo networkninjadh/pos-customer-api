c4986f5fd628ee8f21fd97cfec0f0f1ad34f15b5
networkninjadh <networkninjadh@gmail.com>
2022-04-12 12:18:02 -0500

    initial commit

---
+++ src/main/java/com/howtech/poscustomerapi/PosCustomerApiApplication.java
@@ -0,0 +1,13 @@
+package com.howtech.poscustomerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosCustomerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosCustomerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/howtech/poscustomerapi/PosCustomerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howtech.poscustomerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosCustomerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
